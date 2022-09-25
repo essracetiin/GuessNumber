33f597a143edd383390ba3ef49265e9a4311dbdb
Esra Çetin <essracetiin@gmail.com>
2022-09-25 18:33:59 +0300

    Update

--- HotCold/Program.cs
+++ HotCold/Program.cs
@@ -10,45 +10,48 @@ namespace HotCold
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int number = random.Next(101);
-
-            int? claim = 0;
-            int data = 0;
             while (true)
             {
-                claim++;
-                Console.WriteLine("Please enter a number between 1 and 100");
-                string enteredNumber = Console.ReadLine();
-                bool isValid = Int32.TryParse(enteredNumber, out data);
-                if (isValid)
+                Random random = new Random();
+                int number = random.Next(101);
+
+                int? claim = 0;
+                int data = 0;
+                while (number != data)
                 {
-                    if (data == number)
+                    claim++;
+                    Console.WriteLine("Please enter a number between 1 and 100");
+                    string enteredNumber = Console.ReadLine();
+                    bool isValid = Int32.TryParse(enteredNumber, out data);
+                    if (isValid)
                     {
-                        Console.WriteLine("Congratulations, you got it with {0} guesses", claim);
-                    }
-                    else if ((number - data) <= 3 && (data - number) <= 3)
-                    {
-                        Console.WriteLine("Too Hot!");
-                    }
-                    else if ((data - number) <= 20 && (number - data) <= 20)
-                    {
-                        Console.WriteLine("Hot");
+                        if (data == number)
+                        {
+                            Console.WriteLine("Congratulations, you got it with {0} guesses", claim);
+                        }
+                        else if ((number - data) <= 3 && (data - number) <= 3)
+                        {
+                            Console.WriteLine("Too Hot!");
+                        }
+                        else if ((data - number) <= 20 && (number - data) <= 20)
+                        {
+                            Console.WriteLine("Hot");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Cold");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine("Cold");
+                        Console.WriteLine("Please enter correct value!");
                     }
+
+
+
                 }
-                else
-                {
-                    Console.WriteLine("Please enter correct value!");
-                }
-
-                
-
-
             }
+            
 
 
         }
